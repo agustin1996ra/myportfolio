f8c8570b31b9f4286506d46d530f942330d80f79
agustin1996ra <63318331+agustin1996ra@users.noreply.github.com>
2023-08-13 19:47:27 -0300

    panel de administracion

--- app/posts/admin.py
+++ app/posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Album, Photo
+
+admin.site.register(Album)
+admin.site.register(Photo)
