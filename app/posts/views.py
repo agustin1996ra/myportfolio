ae5fe301c180f700791c264374736d27ceaa9be9
agustin1996ra <63318331+agustin1996ra@users.noreply.github.com>
2023-08-13 18:45:47 -0300

    plantilla proyectos

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return render(request, 'home.html', {})
+    pagina_actual = 'home'
+    return render(request, 'home.html', {'pagina_actual': pagina_actual})
+
+
+def proyectos(request):
+    pagina_actual = 'proyectos'
+    return render(request, 'proyectos.html', {'pagina_actual': pagina_actual})
