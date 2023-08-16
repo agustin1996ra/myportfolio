933fbc1fa367c510eea8276cbc1117a2d4c1d356
agustin1996ra <63318331+agustin1996ra@users.noreply.github.com>
2023-08-16 18:15:46 -0300

    correccion de url

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/proyectos', views.proyectos, name="proyectos")
+    path('proyectos/', views.proyectos, name="proyectos")
 ]
 
